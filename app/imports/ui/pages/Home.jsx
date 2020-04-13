d236ffcdb251f9424bdd37293bd496628e59cdeb
heweiron <42639451+heweiron@users.noreply.github.com>
2020-04-12 17:25:02 -1000

    implement search

--- app/imports/ui/pages/Home.jsx
+++ app/imports/ui/pages/Home.jsx
@@ -8,16 +8,55 @@ import {
   Image,
   Divider,
 } from 'semantic-ui-react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Redirect } from 'react-router-dom';
 
 export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchWords: '',
+      directToList: false,
+    };
+  }
+
+  handleClick() {
+    if (this.state.searchWords !== '') {
+      this.setState({ directToList: true });
+    }
+  }
+
+  handleInputChange = (e) => {
+    this.setState({ searchWords: e.target.value });
+  };
+
+  pressEnter = (event) => {
+    if (event.keyCode === 13) {
+      this.handleClick();
+    }
+  };
+
   render() {
+    if (this.state.directToList) {
+      return <Redirect to={`/list/${this.state.searchWords}`}/>;
+    }
     return (
         <div>
-          <Container className={'topLanding'} fluid textAlign={'center'} style={{ marginTop: '-10px' }}>
-            <Image src={'/images/manoalist-logo.png'} size={'huge'} centered/>
-            <Input className={'searchInput'} size='large' action={{ icon: 'search' }} actionPosition={'left'}
-                   placeholder='Search...'/>
+          <Container className={'topLanding'}
+                     fluid
+                     textAlign={'center'}
+                     style={{ marginTop: '-10px' }}>
+            <Image src={'/images/manoalist-logo.png'}
+                   size={'huge'}
+                   centered/>
+            <Input className={'searchInput'}
+                   size='large'
+                   action={{
+                     icon: 'search',
+                     onClick: () => this.handleClick(),
+                   }}
+                   actionPosition={'left'}
+                   placeholder='Search...'
+                   onChange={this.handleInputChange} onKeyDown={ this.pressEnter }/>
           </Container>
           <Container>
             <Divider hidden/>
@@ -39,8 +78,13 @@ export default class Home extends React.Component {
                 <Image src={'http://clipart-library.com/img/2008785.jpg'}
                        size={'small'}/>
               </Grid.Column>
-              <Grid.Column textAlign={'center'} verticalAlign={'middle'} as={NavLink} exact to={'/list'}>
-                <Header as="h2" content={'SEE ALL'}/>
+              <Grid.Column textAlign={'center'}
+                           verticalAlign={'middle'}
+                           as={NavLink}
+                           exact
+                           to={'/list'}>
+                <Header as="h2"
+                        content={'SEE ALL'}/>
               </Grid.Column>
             </Grid>
             </Segment>
@@ -66,8 +110,13 @@ export default class Home extends React.Component {
                 <Image src={'http://clipart-library.com/img/2008785.jpg'}
                        size={'small'}/>
               </Grid.Column>
-              <Grid.Column textAlign={'center'} verticalAlign={'middle'} as={NavLink} exact to={'/list'}>
-                <Header as="h2" content={'SEE ALL'}/>
+              <Grid.Column textAlign={'center'}
+                           verticalAlign={'middle'}
+                           as={NavLink}
+                           exact
+                           to={'/list'}>
+                <Header as="h2"
+                        content={'SEE ALL'}/>
               </Grid.Column>
             </Grid>
             </Segment>
