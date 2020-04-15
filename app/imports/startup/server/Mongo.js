5e87602025a13a879a09d11d12a3c34693910a57
heweiron <42639451+heweiron@users.noreply.github.com>
2020-04-14 23:35:32 -1000

    finish combine with issue-29

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/Stuff.js';
 import { Items } from '../../api/item/Item';
+import { Categories } from '../../api/category/Category';
 
 /* eslint-disable no-console */
 
@@ -29,5 +30,19 @@ if (Items.find().count() === 0) {
   if (Meteor.settings.defaultItems) {
     console.log('Creating default items.');
     Meteor.settings.defaultItems.map(data => addItems(data));
+  }
+}
+
+/** Initialize the database with a default data document. */
+function addCategories(data) {
+  console.log(`  Adding: ${data.name} (${data.owner})`);
+  Categories.insert(data);
+}
+
+/** Initialize the collection if empty. */
+if (Categories.find().count() === 0) {
+  if (Meteor.settings.defaultCategory) {
+    console.log('Creating default data.');
+    Meteor.settings.defaultCategory.map(data => addCategories(data));
   }
 }
--- app/imports/ui/components/CategoryItem.jsx
+++ app/imports/ui/components/CategoryItem.jsx
@@ -1,27 +1,41 @@
 import React from 'react';
-import { Table } from 'semantic-ui-react';
+import { Meteor } from 'meteor/meteor';
+import { withTracker } from 'meteor/react-meteor-data';
+import { Dropdown } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
-import { withRouter, Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
+import CategoryItemItem from './CategoryItemItem';
+import { Categories } from '../../api/category/Category';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class CategoryItem extends React.Component {
   render() {
+    const names = Categories.find({ group: this.props.group }).fetch();
     return (
-        <Table.Row>
-          <Table.Cell>{this.props.category.group}</Table.Cell>
-          <Table.Cell>{this.props.category.name}</Table.Cell>
-          <Table.Cell>
-            <Link to={`/edit/${this.props.category._id}`}>Edit</Link>
-          </Table.Cell>
-        </Table.Row>
+        <Dropdown.Item>
+          <a style={{ color: 'black' }} href={`#/list/${this.props.group}/null`}>{this.props.group}</a>
+          <Dropdown>
+            <Dropdown.Menu>
+              {names.map((category) => <CategoryItemItem key={category._id} category={category}/>)}
+            </Dropdown.Menu>
+          </Dropdown>
+        </Dropdown.Item>
+
     );
   }
 }
 
 /** Require a document to be passed to this component. */
 CategoryItem.propTypes = {
-  category: PropTypes.object.isRequired,
+  group: PropTypes.string.isRequired,
+  categories: PropTypes.array.isRequired,
 };
-
+const subscription = Meteor.subscribe('Categories');
+/** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
+const CategoryItemContainer = withTracker(() => ({
+  currentUser: Meteor.user() ? Meteor.user().username : '',
+  categories: Categories.find({}).fetch(),
+  ready: subscription.ready(),
+}))(CategoryItem);
 /** Wrap this component in withRouter since we use the <Link> React Router element. */
-export default withRouter(CategoryItem);
+export default withRouter(CategoryItemContainer);
