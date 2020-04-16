a9d31cc751946dbd3c7bffc6c1f381c3de9eefa9
CraigOpie <CraigOpie@Gmail.com>
2020-04-16 03:12:04 -1000

    On branch issue-044  Your branch is up to date with 'upstream/issue-044'.
     Changes to be committed:
    	modified:   app/imports/ui/components/Footer.jsx
    Completed the change to make the register button only present when the user is not logged in.  Put a welcome message in it's place for now.  We could easily replace this with contact us link in the future."

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,27 +1,23 @@
 import React from 'react';
-import {
-  Image,
-  Grid,
-  List,
-  Divider,
-  Icon,
-  Header,
-  Button,
-} from 'semantic-ui-react';
+import { Container, Image, Grid, List, Divider, Icon, Header, Button } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { Meteor } from 'meteor/meteor';
+import { withTracker } from 'meteor/react-meteor-data';
+import { withRouter } from 'react-router-dom';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
-    const divStyle = { margonTop: '15px', backgroundColor: '#D4D3D3' };
+    const divStyle = { marginTop: '15px', backgroundColor: '#D4D3D3' };
     return (
         <footer style={divStyle}>
           <Divider/>
             <Grid columns={3} container>
               <Grid.Column>
-                <Image src={'/images/manoalist-logo.png'} size={'large'}/>
+                <Image src={'/images/manoalist-logo.png'} size={'medium'}/>
               </Grid.Column>
               <Grid.Column>
-                <List size={'huge'}>
+                <List size={'large'}>
                 <List.Item><a href={'/#/about'}>About US
                 </a></List.Item>
                   <List.Item><a href={'/#'}>Home</a></List.Item>
@@ -31,8 +27,14 @@ class Footer extends React.Component {
               </List>
               </Grid.Column>
               <Grid.Column>
-                <Header as={'h2'}>NOT A MEMBER?</Header>
-                <Button content={'Register'} size={'huge'} color={'green'}/>
+                {this.props.currentUser ? (
+                  <Header as={'h3'}>Welcome {this.props.currentUser}</Header>
+                ) : (
+                  <Container>
+                  <Header as={'h3'}>NOT A MEMBER?</Header>
+                  <Button content={'Register'} size={'huge'} color={'green'}/>
+                  </Container>
+                )}
               </Grid.Column>
             </Grid>
         </footer>
@@ -40,4 +42,13 @@ class Footer extends React.Component {
   }
 }
 
-export default Footer;
+/** Declare the types of all properties. */
+Footer.propTypes = {
+  currentUser: PropTypes.string,
+};
+
+const FooterContainer = withTracker(() => ({
+  currentUser: Meteor.user() ? Meteor.user().username : '',
+}))(Footer);
+
+export default withRouter(FooterContainer);
