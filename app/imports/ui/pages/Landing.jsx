7964cdc3bff6e60d2f66a2d19b01ee41901ab0d7
heweiron <42639451+heweiron@users.noreply.github.com>
2020-04-20 14:31:09 -1000

    fix eslint error

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Container, Divider, Grid, Header, Icon, Image, Tab, Menu, Step } from 'semantic-ui-react';
+import { Button, Container, Divider, Grid, Header, Icon, Image, Tab, Menu } from 'semantic-ui-react';
 import { NavLink } from 'react-router-dom';
 
 export default class Landing extends React.Component {
