1ff160fcdd9b2ddd764dcc4fabf05e61e3d7f9b8
CraigOpie <CraigOpie@Gmail.com>
2020-04-25 18:15:13 -1000

    Fixed ES Lint issues

--- app/imports/ui/pages/Signout.jsx
+++ app/imports/ui/pages/Signout.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Redirect } from 'react-router-dom';
+import { Header } from 'semantic-ui-react';
 
 /** After the user clicks the "Signout" link in the NavBar, log them out and display this page. */
 export default class Signout extends React.Component {
   render() {
     Meteor.logout();
+    setTimeout(() => {
+      if (window.location.href.includes('signout')) {                         // eslint-disable-line
+        window.location.href = window.location.href.replace('signout', '');   // eslint-disable-line
+      }
+    }, 2000);
     return (
-      <Redirect to={'/'}/>
+        <Header as="h2" textAlign="center">
+          <p>You are signed out. Redirecting to home page...</p>
+        </Header>
     );
   }
 }
