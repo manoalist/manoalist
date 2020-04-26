263571a8f9026d1e4b23402e0faf7b5aed3c8c94
tianhuizhou <tianhui@hawaii.edu>
2020-04-25 17:55:04 -1000

    delete references
    safe delete all the Stuff references.

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,25 +1,9 @@
 import { Meteor } from 'meteor/meteor';
-import { Stuffs } from '../../api/stuff/Stuff.js';
 import { Items } from '../../api/item/Item';
 import { Categories } from '../../api/category/Category';
 import { User } from '../../api/user/User';
 
 /* eslint-disable no-console */
-
-/** Initialize the database with a default data document. */
-function addData(data) {
-  console.log(`  Adding: ${data.name} (${data.owner})`);
-  Stuffs.insert(data);
-}
-
-/** Initialize the collection if empty. */
-if (Stuffs.find().count() === 0) {
-  if (Meteor.settings.defaultData) {
-    console.log('Creating default data.');
-    Meteor.settings.defaultData.map(data => addData(data));
-  }
-}
-
 /** Initialize the database with a default data document. */
 function addUser(data) {
   console.log(`  Adding: ${data.email}`);
