e9a41244f30b7831e319eea55e793701854c0ed8
heweiron <42639451+heweiron@users.noreply.github.com>
2020-04-26 23:10:37 -1000

    Update AdminApproveItem.jsx

--- app/imports/ui/components/AdminApproveItem.jsx
+++ app/imports/ui/components/AdminApproveItem.jsx
@@ -2,21 +2,26 @@ import React from 'react';
 import { Segment, Header, Button } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
+import { Items } from '../../api/item/Item';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class AdminApproveItem extends React.Component {
+  approveItem =() => Items.update({ _id: this.props.item._id }, { $set: { approvedForSale: 'true' } });
+
+  deleteItem = () => Items.remove({ _id: this.props.item._id });
+
   render() {
     return (
-      <Segment>
-        <Header as='h4'>
-          {this.props.item.name}
-          <Button floated='right' color='green' size='tiny'>Approve</Button>
-          <Button floated='right' size='tiny'>Delete</Button>
-          <Header.Subheader>
-            {this.props.item.owner}
-          </Header.Subheader>
-        </Header>
-      </Segment>
+        <Segment>
+          <Header as='h4'>
+            {this.props.item.name}
+            <Button floated='right' color='green' size='tiny' onClick={this.approveItem}>Approve</Button>
+            <Button floated='right' size='tiny' onClick={this.deleteItem}>Delete</Button>
+            <Header.Subheader>
+              {this.props.item.owner}
+            </Header.Subheader>
+          </Header>
+        </Segment>
     );
   }
 }
