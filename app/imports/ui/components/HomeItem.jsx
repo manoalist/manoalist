8519bbd65dec50492e684229c5efdc187472034a
Edwin <ezheng@hawaii.edu>
2020-04-26 21:20:32 -1000

    Homepage items redirect to respective item page

--- app/imports/ui/components/HomeItem.jsx
+++ app/imports/ui/components/HomeItem.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Image, Grid, Container, Header } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class HomeItem extends React.Component {
   render() {
     return (
         <Grid.Column textAlign={'center'}>
-          <Container style={{ height: '100px' }}>
-            <Image centered
-                   src={this.props.item.picture}
-                   style={{ maxHeight: '100px' }}/>
-          </Container>
-          <Header as={'h4'}>{this.props.item.name}</Header>
+          <Link to={`/details/${this.props.item._id}`}>
+            <Container style={{ height: '100px' }}>
+              <Image centered
+                    src={this.props.item.picture}
+                    style={{ maxHeight: '100px' }}/>
+            </Container>
+            <Header as={'h4'}>{this.props.item.name}</Header>
+          </Link>
         </Grid.Column>
     );
   }
