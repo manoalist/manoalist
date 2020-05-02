28b5c97cebc961ff3cb57039c08c38264d77bbbb
heweiron <42639451+heweiron@users.noreply.github.com>
2020-05-02 04:00:45 -1000

    improve rating ui in profile

--- app/imports/ui/pages/Profile.jsx
+++ app/imports/ui/pages/Profile.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import _ from 'lodash';
-import { Grid, Comment, Loader, Header, Segment, Container } from 'semantic-ui-react';
+import { Grid, Comment, Loader, Header, Segment, Container, Statistic,
+  Rating, Progress, List } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withTracker } from 'meteor/react-meteor-data';
 import UserInfo from '../components/UserInfo';
@@ -88,16 +89,72 @@ class Profile extends React.Component {
               { this.renderDashboard() }
               <Grid.Row className='ratings-box'>
                   <Segment>
-                      <Grid>
-                          <Grid.Row centered>
-                            <h3>Ratings</h3>
-                          </Grid.Row>
-                            {ratings.length > 0 ? <Comment.Group size={'big'}>{this.props.ratings
-                                    .map((rating, index) => <RatingItem rating={rating} key={index}/>)}
-                                </Comment.Group>
-                                : <Header as={Container} textAlign={'center'}>There is no rating for you</Header>}
-                      </Grid>
-                  </Segment>
+                  <Grid>
+                    <Grid.Row centered>
+                      <h3>Ratings</h3>
+                    </Grid.Row>
+                    <Grid.Row columns={2}>
+                      <Grid.Column>
+                        <h3>{ratings.length} User Ratings</h3>
+                        {ratings.length > 0 ? <Comment.Group size={'big'}>{this.props.ratings
+                            .map((rating, index) => <RatingItem rating={rating}
+                                                                key={index}/>)}
+                        </Comment.Group>
+                        : <Header as={Container}
+                                  textAlign={'center'}>There is no rating for you</Header>}
+                      </Grid.Column>
+                      <Grid.Column>
+                        <Statistic>
+                          <Statistic.Label>Average Rating</Statistic.Label>
+                          <Statistic.Value>
+                            {(Math.round((ratings
+                                    .reduce((pre, cur) => pre + cur.rating, 0)
+                                / ratings.length) * 10) / 10).toString()}
+                          </Statistic.Value>
+                        </Statistic>
+                        <Rating maxRating={5}
+                                   defaultRating={Math.round((ratings
+                                           .reduce((pre, cur) => pre + cur.rating, 0)
+                                       / ratings.length) * 10) / 10}
+                                   icon='star'
+                                   style={{ marginLeft: '1em' }}
+                                   size='massive'
+                                   disabled/>
+                      <List><List.Item>
+                            <h3>5 Star</h3>
+                              <Progress
+                                  percent={(Math.round((ratings.filter(rating => rating.rating === 5).length
+                                      / ratings.length) * 100) / 100) * 100}
+                                  inverted color='red' progress size={'medium'}/>
+                                   </List.Item>
+                          <List.Item>
+                            <h3>4 Star</h3>
+                            <Progress
+                                percent={(Math.round((ratings.filter(rating => rating.rating === 4).length
+                                / ratings.length) * 100) / 100) * 100} inverted color='orange' progress/>
+                          </List.Item>
+                          <List.Item>
+                            <h3>3 Star</h3>
+                            <Progress
+                                percent={(Math.round((ratings.filter(rating => rating.rating === 3).length
+                                / ratings.length) * 100) / 100) * 100} inverted color='yellow' progress/>
+                          </List.Item>
+                          <List.Item>
+                            <h3>2 Star</h3>
+                            <Progress
+                                percent={(Math.round((ratings.filter(rating => rating.rating === 2).length
+                                / ratings.length) * 100) / 100) * 100} inverted color='olive' progress/>
+                          </List.Item>
+                          <List.Item>
+                            <h3>1 Star</h3>
+                            <Progress
+                                percent={(Math.round((ratings.filter(rating => rating.rating === 1).length
+                                / ratings.length) * 100) / 100) * 100} inverted color='green' progress/>
+                          </List.Item>
+                        </List>
+                      </Grid.Column></Grid.Row>
+                  </Grid>
+                </Segment>
               </Grid.Row>
           </Grid.Column>
         </Grid>
